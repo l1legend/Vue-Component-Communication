5f32d4c31ade37b55e71f2e78f78f933dcb0284c
minTwin <michaelmin2020@gmail.com>
2022-01-21 09:55:49 -0600

    Initial Commit

---
+++ cmp-communication-admin-file-managment-prototype/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <section>
+    <header>
+      <h1>Admin File Management Panel</h1>
+    </header>
+    <ul>
+      <friend-contact
+        name="file1.pdf"
+        phone-number="111 111 1111"
+        email-address="manuel@localhost.com"
+        is-favorite="1"
+        description="some data"    
+      ></friend-contact>
+      <friend-contact
+        name="file2.pdf"
+        phone-number="222 222 2222"
+        email-address="@localhost.com"
+        is-favorite="1"
+        description="some data 2"   
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: "Jost", sans-serif;
+}
+body {
+  margin: 0;
+}
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ cmp-communication-admin-file-managment-prototype/src/components/FriendContact.vue
@@ -0,0 +1,51 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ fileIsPublic === '1' ? '(Public)' : '(Private)'}}</h2>
+    <button @click="togglePublic">Toggle Public or Private</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber}}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+            <li>
+        <strong>Description</strong>
+        {{ description }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress',
+    'isPublic',
+    'description'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      fileIsPublic: this.isPublic
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    togglePublic() {
+      if (this.fileIsPublic === '1') {
+        this.fileIsPublic = '0';
+      }else {
+        this.fileIsPublic = '1';
+      }
+    },
+  }
+};
+</script>
